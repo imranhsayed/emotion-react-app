b34e7bf7a8388f442ada33ef026b742cf20f733c
Imran Sayed <imranhsayed@gmail.com>
2019-03-21 18:31:16 +0530

    Create Emotion styled components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import styled from '@emotion/styled';
+import colors from './colors/colors';
+
+const Container = styled( 'header' )`
+  background-color: ${ colors.dark };
+  position: sticky;
+  top: 0;
+  
+  span {
+ 	color: ${ colors.primary };
+  }
+`;
+
+const NavLink = styled( Link )`
+
+	color: #fff;
+	
+	&:hover {
+	color: ${ colors.primary };
+	}
+`;
 
 export default class NavBar extends React.Component {
 	render() {
 		return(
-			<header>
-				<Link to="/">Home</Link>
-			</header>
+			<Container>
+				<NavLink to="/">Home</NavLink>
+				<span>Test</span>
+			</Container>
 		)
 	}
 }
